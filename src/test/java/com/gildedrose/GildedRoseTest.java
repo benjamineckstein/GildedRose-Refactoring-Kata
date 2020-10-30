99ad781841484c16de63f2b74a9b93e1b3f96122
Benjamin Eckstein <>
2020-10-30 12:50:12 +0100

    "mob next [ci-skip]"

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,5 +13,90 @@ class GildedRoseTest {
         app.updateQuality();
         assertThat(app.items[0].name).isEqualTo("foo");
     }
+
+    @Test
+    void qualityShouldDegrade() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(9);
+    }
+
+    @Test
+    void sellInShouldDegrade() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn).isEqualTo(9);
+    }
+
+    @Test
+    void qualityShouldDegradeSecondTime() {
+        Item[] items = new Item[] { new Item("foo", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(8);
+    }
+
+    @Test
+    void qualityShouldDegradeTwiceWhenSellInHasPassed() {
+        Item[] items = new Item[] { new Item("foo", 0, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+    @Test
+    void qualityShouldNeverBeNegative() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+
+    @Test
+    void qualityIncreaseOverTimeForAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(11);
+    }
+
+
+    @Test
+    void qualityShouldNotBeOver50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(50);
+    }
+
+
+    @Test
+    @Disabled
+    void qualityShouldBeLoweredToAtLeast50() {
+        Item[] items = new Item[] { new Item("foo", 10, 52) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(50);
+    }
+
+
+    @Test
+    void sulfurasShouldNotDecreaseInSellinOrQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 30, 40) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn).isEqualTo(30);
+        assertThat(app.items[0].quality).isEqualTo(40);
+    }
+
+
+
+
+
+
 
 }
