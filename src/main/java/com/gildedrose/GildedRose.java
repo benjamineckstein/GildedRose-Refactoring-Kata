dfd8ee5509bef3ae265dda018e3fddcd28b666fc
Benjamin Eckstein <>
2020-10-31 14:44:45 +0100

    added challenges

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,12 +6,20 @@ class GildedRose {
 
     /**
      * Do not change the arguments of the constructor in any way
+     *
      * @param items Array of Items
      */
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    /**
+     * Available challenges:
+     * 1. Do not use your mouse at all
+     * 2. No breaking changes. Only baby steps are allowed and tests must pass at any time.
+     * 3. Explain what your goal is and why this is good before doing it.
+     * 4. You are only allowed to speak, someone else has to do it.
+     */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
