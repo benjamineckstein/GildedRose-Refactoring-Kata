d2e7a96102703bb64167ad1be9a047ec4f76b882
Benjamin Eckstein <>
2020-11-13 15:18:02 +0100

    RefactorSession1

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,55 +22,47 @@ class GildedRose {
      * 5. Bonus Challenge: Refactor in a way that you do not use a single if statement
      */
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+        for (Item item : items) {
+            updateItem(item);
         }
     }
+
+    private void updateItem(Item item) {
+        item.quality =
+                switch (item.name) {
+                    case "Aged Brie" -> item.quality + 1;
+                    case "Backstage passes to a TAFKAL80ETC concert" -> item.quality + updateItemQualityBackstage(item);
+                    case "Sulfuras, Hand of Ragnaros" -> item.quality;
+                    default -> item.quality - 1;
+                };
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            item.quality = switch (item.name) {
+                case "Aged Brie" -> item.quality +1;
+                case "Backstage passes to a TAFKAL80ETC concert" -> 0;
+                case "Sulfuras, Hand of Ragnaros" -> item.quality;
+                default -> item.quality - 1;
+            };
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality = Math.max(item.quality, 0);
+            item.quality = Math.min(50, item.quality);
+        }
+    }
+
+    private int updateItemQualityBackstage(Item item) {
+        int result = 1;
+        if (item.sellIn < 6) {
+            result = 3;
+        } else if (item.sellIn < 11) {
+            result = 2;
+        }
+        return result;
+    }
+
 }
